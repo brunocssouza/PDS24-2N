9b09525c7e8d026f990dc4c89744df186b40d47b
Bruno César <92397493+brunocssouza@users.noreply.github.com>
2024-12-06 20:23:25 -0300

    Add files via upload

---
+++ 06-12/Operadores Eletronicos.js
@@ -0,0 +1,71 @@
+let a;
+let b;
+let resultado;
+
+/* NOT (!) */
+a = false;
+resultado = !a;
+
+console.log(`!${a} = ${resultado}`);
+
+/* AND (&&) */
+a = true;
+b = false;
+resultado = a && b;
+
+console.log(`${a} && ${b} = ${resultado}`);
+
+/* OR (||) */
+a = true;
+b = false;
+resultado = a || b;
+
+console.log(`${a} || ${b} = ${resultado}`);
+
+/* EQUAL (==) */
+a = 1;
+b = 2;
+resultado = a == b;
+
+console.log(`${a} == ${b} = ${resultado}`);
+
+/* DIFFERENT != */
+a = 1;
+b = 2;
+resultado = a != b;
+
+console.log(`${a} != ${b} = ${resultado}`);
+
+/* GREATER THAN (>) */
+a = 1;
+b = 2;
+resultado = a > b;
+
+console.log(`${a} > ${b} = ${resultado}`);
+
+/* lESSER THAN (<) */
+a = 1;
+b = 2;
+resultado = a < b;
+
+console.log(`${a} < ${b} = ${resultado}`);
+
+/* GREATER OR EQUAL THAN (>=) */
+a = 1;
+b = 2;
+resultado = a >= b;
+
+console.log(`${a} <= ${b} = ${resultado}`);
+
+/* LESSER OR EQUAL THAN (<=) */
+a = 1;
+b = 2;
+resultado = a <= b;
+
+console.log(`${a} <= ${b} = ${resultado}`);
+
+
+
+
+
+
